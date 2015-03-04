d9ee02ad407ebf49eacddc97b201837cdd9df641
J. Thompson <Jibri.Thompson@loop.colum.edu>
2015-03-04 13:48:19 -0600

    This is js for quantity values
    must be placed in correct directory

---
+++ quantity.js
@@ -0,0 +1,15 @@
+var a = document.querySelectorAll('input[type=button]');
+
+for (var i = 0; i<a.length; i++) {
+ a[i].addEventListener('click', increaseDecrease);   
+}
+
+function increaseDecrease () {
+    var b = document.getElementById('textBox');
+    if (this.id == 'decreaseNumber') {
+    b.value = parseInt(b.value) - 1;
+    }
+    else if (this.id == 'increaseNumber') {
+    b.value = parseInt(b.value) + 1;                                      
+    }
+}
